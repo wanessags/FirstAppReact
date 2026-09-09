a64a96b5ac4639828e8b0ff751b13014611bd792
WANESSA GONCALVES DE SOUZA <wanessa.gsouza@senacsp.edu.br>
2026-09-08 21:19:21 -0300

    style: add first react app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
 import ReactLogo from './assets/react-logo.png'
+import './style.css'
 
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering"
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)"
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there"
+  ]
+];
+
+console.log(content[0]);
+console.log(content[3]);
 
 function App() {
   return (
