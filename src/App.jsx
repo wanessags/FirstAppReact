08c93d70a125fe9e36b72be9a4fe566d59cda1dc
WANESSA GONCALVES DE SOUZA <wanessa.gsouza@senacsp.edu.br>
2026-09-08 22:01:40 -0300

    feat: highlight active tab

--- src/App.jsx
+++ src/App.jsx
@@ -45,14 +45,21 @@ export default function App() {
 
       <div id="tabs">
         <menu>
-          <button onClick={() => setActiveContentIndex(0)}>
+          <button className={activeContentIndex === 0 ? "active" :""}
+           onClick={() => setActiveContentIndex(0)}>
             Why React?
           </button>
-          <button onClick={() => setActiveContentIndex(1)}>
+          <button className={activeContentIndex === 1 ? "active" : ""}
+          onClick={() => setActiveContentIndex(1)}>
             Core Features
           </button>
-          <button onClick={() => setActiveContentIndex(2)}>
+          <button  className={activeContentIndex === 2 ? "active" : ""}
+           onClick={() => setActiveContentIndex(2)}>
             Related Resources
+          </button>
+          <button  className={activeContentIndex === 3 ? "active" : ""}
+           onClick={() => setActiveContentIndex(3)}>
+            React vs. Vanilla
           </button>
         </menu>
 
